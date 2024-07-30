fc4e75b21b7e2ea76dccaa9b54a47d7d27bda61b
mikdaad <mikxplode@gmail.com>
2024-07-30 21:26:23 +0530

    seventh

--- src/Components/swipe.js
+++ src/Components/swipe.js
@@ -2,32 +2,20 @@ import React, { useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import "../Styles/swipe.css";
 
-const TextSwiper = ({ textString }) => {
-  const maxDots = 3;
-    const text =   typeof textString === 'string' && textString.trim() !== '' 
+const TextSwiper = ({ textString, maxChars = 500 }) => {
+  const text = typeof textString === 'string' && textString.trim() !== ''
     ? (() => {
-        let dotCount = 0;
-        let lastSplitIndex = 0;
         const parts = [];
+        let lastSplitIndex = 0;
 
-        for (let i = 0; i < textString.length; i++) {
-            if (textString[i] === '.') {
-                dotCount++;
-                if (dotCount === maxDots) {
-                    parts.push(textString.slice(lastSplitIndex, i));
-                    lastSplitIndex = i + 1; // Update last split index to the character after the dot
-                    dotCount = 0; // Reset dot count
-                }
-            }
+        while (lastSplitIndex < textString.length) {
+          parts.push(textString.slice(lastSplitIndex, lastSplitIndex + maxChars));
+          lastSplitIndex += maxChars;
         }
 
-        // Add the remaining part of the string after the last split
-        parts.push(textString.slice(lastSplitIndex));
-
         return parts;
-    })() 
+      })()
     : [];
-  console.log(text);
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -35,13 +23,13 @@ const TextSwiper = ({ textString }) => {
     onSwipedLeft: () => setCurrentIndex((prevIndex) => (prevIndex + 1) % text.length),
     onSwipedRight: () => setCurrentIndex((prevIndex) => (prevIndex - 1 + text.length) % text.length),
     preventDefaultTouchmoveEvent: true,
-    trackMouse: true
+    trackMouse: true,
   });
 
   return (
     <div {...handlers} className="swiper-container">
       <div className="card">
-        <p >{text[currentIndex]}</p>
+        <p>{text[currentIndex]}</p>
       </div>
       <div className="pagination">
         {currentIndex + 1} / {text.length}
